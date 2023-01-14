b72ea630a8fdeba07f371e850eb8fe46dac9070b
Chris Soliday <solidaycl@gmail.com>
2023-01-13 19:30:29 -0500

    more clojure fun

--- src/mastadog/core.clj
+++ src/mastadog/core.clj
@@ -1,7 +1,17 @@
 (ns mastadog.core
   (:gen-class))
 
-(defn -main
+
+(defn myfunc
+  "some doc string"
+  [x]
+  x)
+
+(defn main3
   "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  []
+  (prn "Hello, World!")
+  (myfunc "arg1")) 
+
+(main3)
+(myfunc "hello")
